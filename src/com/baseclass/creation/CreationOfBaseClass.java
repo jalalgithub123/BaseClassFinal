6509dca41fed1d77ef421d3bd40e782132614918
jalal8068 <jalalahamad9@gmail.com>
2019-02-13 22:55:36 +0530

    base class commit

---
+++ src/com/baseclass/creation/CreationOfBaseClass.java
@@ -0,0 +1,12 @@
+package com.baseclass.creation;
+
+public class CreationOfBaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+public void methodA() {
+	System.out.println("MethodA");
+}
+}
